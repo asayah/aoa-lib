beb37e59128b5d464534fe773e0e8e42c33716e2
asayah <adam@solo.io>
2023-01-23 09:45:55 -0700

    refactoring deploy script

--- aoa-tools/deploy.sh
+++ aoa-tools/deploy.sh
@@ -1,33 +1,103 @@
 #!/bin/bash
 #set -e
 
-export vars_file=$1
 
-# check to see if environment name variable was passed through, if not prompt for it
-if [[ ${vars_file} == "" ]]
+############################################################
+# Defaults
+environment_overlay=base
+install_infra=false
+############################################################
+
+
+############################################################
+# Help                                                     #
+############################################################
+help()
+{
+   # Display Help
+   echo "AoA installer."
+   echo
+   echo "Syntax: ./deploy [-f|-o|-i|-h]"
+   echo "options:"
+   echo "-f     path to AoA files"
+   echo "-o     overlay"   
+   echo "-i     install infra"
+   echo "-h     print help"
+   echo
+}
+
+precheck()
+{
+
+echo "##########################"
+echo "    AoA Lib - Precheck    "
+echo "Environemnt: $env"
+echo "Install infra: $install_infra"
+echo "Overlay: $environment_overlay"
+echo "##########################"
+echo ""
+echo "Continue? [Y/N]"
+
+read should_continue
+if [[ ${should_continue} != "Y" ]]
+  then
+   exit 0
+fi
+   
+}
+
+
+check_env()
+{
+  if [[ ${env} == "" ]] || [ ! -d "$env" ]
   then
     # provide vars file
-    echo "Please provide the vars file to use (i.e. single-cluster-freestyle-vars.txt):"
-    read vars_file
-fi
+    echo "Error: env folder not found, please use -f to choose a valid env folder."
+    help
+    exit 1
+   fi 
+}
 
-#pushd . > '/dev/null';
-#SCRIPT_DIR="${BASH_SOURCE[0]:-$0}";
-#
-#while [ -h "$SCRIPT_DIR" ];
-#do
-#    cd "$( dirname -- "$SCRIPT_DIR"; )";
-#    SCRIPT_DIR="$( readlink -f -- "$SCRIPT_DIR"; )";
-#done
-#
-#cd "$( dirname -- "$SCRIPT_DIR"; )" > '/dev/null';
-SCRIPT_DIR="$( pwd; )/aoa-tools";
-#popd  > '/dev/null';
+install_infra()
+{
+   check_env
+   export install_infra=true
+}
 
-# source vars from root directory vars.txt
-PARENT_DIR="$(dirname "$SCRIPT_DIR")"
-source $PARENT_DIR/${vars_file} && export $(sed '/^#/d' $PARENT_DIR/${vars_file} | cut -d= -f1)
 
+############################################################
+# Get the options
+while getopts "hif:o:" option; do
+   case $option in
+      h) # display Help
+         help
+         exit;;
+      f) # env
+         env=${OPTARG};;
+      i) # infra
+         install_infra;;
+      o) # overlay
+         environment_overlay=${OPTARG};;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         help
+         exit;;
+   esac
+done
+
+check_env
+precheck
+
+export SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $env
+git_root="$(git rev-parse --show-toplevel)/"
+export env_path=$(echo $PWD | sed -e "s+$git_root++g")
+
+# source vars.env 
+source vars.env && export $(sed '/^#/d' vars.env | cut -d= -f1)
+
+#TODO: this should be in a init phase
 # check to see if defined contexts exist
 if [[ $(kubectl config get-contexts | grep ${cluster_context}) == "" ]] ; then
   echo "Check Failed: ${cluster_context} context does not exist. Please check to see if you have the clusters available"
@@ -44,26 +114,27 @@ if [[ ${environment_overlay} == "" ]]
 fi
 
 # install argocd
+#TODO: refactor the install-argocd.sh to no relay on cd
 cd $SCRIPT_DIR/bootstrap-argocd
 if [ "${environment_overlay}" == "ocp" ] ; then 
-     ./install-argocd.sh insecure-rootpath-ocp ${cluster_context}
+     $SCRIPT_DIR/bootstrap-argocd/install-argocd.sh insecure-rootpath-ocp ${cluster_context}
   else
-     ./install-argocd.sh insecure-rootpath ${cluster_context}
+     $SCRIPT_DIR/bootstrap-argocd/install-argocd.sh insecure-rootpath ${cluster_context}
   fi
-cd $SCRIPT_DIR
 
 # wait for argo cluster rollout
 $SCRIPT_DIR/tools/wait-for-rollout.sh deployment argocd-server argocd 20 ${cluster_context}
 
+cd $env
 # deploy app of app waves
-for i in $(ls $PARENT_DIR/environments/${environment_name} | sort -n); do 
+for i in $(ls | sort -n); do 
   echo "starting ${i}"
   # run init script if it exists
-  [[ -f "$PARENT_DIR/environments/${environment_name}/${i}/init.sh" ]] && $PARENT_DIR/environments/${environment_name}/${i}/init.sh 
+  [[ -f "${i}/init.sh" ]] && ${i}/init.sh 
   # deploy aoa wave
-  $SCRIPT_DIR/tools/configure-wave.sh ${environment_name} ${i} ${environment_overlay} ${cluster_context} ${github_username} ${repo_name} ${target_branch}
+  $SCRIPT_DIR/tools/configure-wave.sh ${env_path} ${i} ${environment_overlay} ${cluster_context} ${github_username} ${repo_name} ${target_branch}
   # run test script if it exists
-  [[ -f "$PARENT_DIR/environments/${environment_name}/${i}/test.sh" ]] && $PARENT_DIR/environments/${environment_name}/${i}/test.sh
+  [[ -f "${i}/test.sh" ]] && ${i}/test.sh
 done
 
 echo "END."
